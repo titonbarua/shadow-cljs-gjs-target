efd28347678d2590bfe6e6254fccb62679639b0b
Titon Barua <titon@vimmaniac.com>
2020-06-26 13:22:25 +0600

    Imported code from `https://github.com/thheller/shadow-cljs/pull/730` into a separte project, as suggested.

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject titonbarua/shadow-cljs-gjs-backend "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies []
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.10.1"]
+                                       [thheller/shadow-cljs "2.10.13"]]}}
+  :repl-options {:init-ns shadow-cljs-gjs-backend.core})
