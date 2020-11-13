ad194ba3992f53a302a61fe6324ec4aa8730a5b0
Eugen Stan <eugen.stan@netdava.com>
2020-11-13 12:05:20 +0200

    Introduced a special target for gnome-extensions
    * It's a copy of core but without a wrapping funcion, without preamble
    * It binds global variables (hardcoded names: init, enable, disable) - we can clean this up

--- examples/gnome-extension-demo/src/main/extension.cljs
+++ examples/gnome-extension-demo/src/main/extension.cljs
@@ -1,20 +1,46 @@
-(ns extension)
+(ns extension
+  ;;  npx shadow-cljs release app => --- shadow/cljs/module_vars/main.js:1 \n Parse error. Semi-colon expected
+  ;; (:require ["gjs.misc.extensionUtils" :as ExtensionUtils])
+  (:require [goog.object :as g]))
 
-;; class Extension {constructor () {}
-;;                  enable () {}
-;;                  disable () {}}
-;;                  
-;; function init () {
-;;    return new Extension ();
+;; Objective: 
+;; 
+;; https://gjs.guide/extensions/overview/anatomy.html
+;; 
+;; const ExtensionUtils = imports.misc.extensionUtils;
+;; const Me = ExtensionUtils.getCurrentExtension();
+;; 
+;; function init() {
+;;     log(`initializing ${Me.metadata.name}`);
+;; }
+;; 
+;; function enable() {
+;;     log(`enabling ${Me.metadata.name}`);
+;; }
+;; 
+;; function disable() {
+;;     log(`disabling ${Me.metadata.name}`);
 ;; }
 
+(def ExtensionUtils js/imports.misc.extensionUtils)
+(def Me (.getCurrentExtension ExtensionUtils))
+
+
+(defn init []
+  (js/log "clojurescript-demo initialized")
+  (js/log (.. Me -metadata -name))
+  nil)
+
 (defn enable []
-  (println "Enable extension"))
+  (js/log "Enable clojurescript-demo extension")
+  (js/log (.. Me -metadata -name))
+  nil)
 
 (defn disable []
-  (println "Disable extension"))
+  (js/log "Disable clojurescript-demo extension")
+  (js/log (.. Me -metadata -name))
+  nil)
 
-(defn ^:export init 
-  []
-  {:enable enable
-   :disable disable})
+(g/set js/global "init" init)
+(g/set js/global "enable" enable)
+(g/set js/global "disable" disable)
